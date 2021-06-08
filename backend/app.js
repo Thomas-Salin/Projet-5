1884de9ce80e9bcc60388254ce667463a9bcd11e
Thomas Salin <salin.thomas.pro@gmail.com>
2021-06-08 20:28:14 +0200

    Integration de la BDD mongoDB et création modele sauce

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://Thomas:21102019@cluster0.kzh6r.mongodb.net/Cluster0?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//* Middleware correspondant au body-parser afin de récuperer l'objet json de la requete post du frontend *//
+app.use(bodyParser.json());
+
+//* middleware correspondant au CORS *//
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 module.exports = app;
